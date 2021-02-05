84675efc07893b4db5b7b651d9040ea9609838c8
litter-rabbit <709343607@qq.com>
2021-02-05 15:58:02 +0800

    更新环境变量

--- setting.py
+++ setting.py
@@ -1,7 +1,8 @@
 import os
 
 port = 8888
-debug = True
+
+debug = eval(os.environ.get('DEBUG',False))
 db_host= '127.0.0.1'
 db_port =3306
 db_user='root'
@@ -15,3 +16,9 @@ static_path = os.path.join(os.path.dirname(__file__), 'static')
 smtp_server='smtp.qq.com'
 email_admin='709343607@qq.com'
 email_password="aochpsyudjiibfia"
+
+def test():
+    debug = os.getenv('DEBUG')
+    print(debug)
+
+
